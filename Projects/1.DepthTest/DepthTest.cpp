064cc489f1f5ab4fe53a50dcf49a53a28686ba18
seal2002 <hai.phannhut@gameloft.com>
2017-07-24 23:26:57 +0700

    Add Resource / Draw Cube

--- Projects/1.DepthTest/DepthTest.cpp
+++ Projects/1.DepthTest/DepthTest.cpp
@@ -3,17 +3,67 @@
 #include <glm\common.hpp>
 #include <glm\gtc\matrix_transform.hpp>
 #include <GLFW\include\GLFW\glfw3.h>
+#include <stb_image.h>
 #include "common\Shader.h"
 #include "common\Camera.h"
+
 
 using namespace OpenGLWindow;
 
 void do_movement();
+unsigned int loadTexture(const char *path);
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 bool* keys;
 
 static float SCR_W = 800.0f;
 static float SCR_H = 600.0f;
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+float cubeVertices[] = {
+    // positions          // texture Coords
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
 
 int main()
 {
@@ -21,14 +71,129 @@ int main()
 
     glEnable(GL_DEPTH_TEST);
 
-    Shader ourShader("..\\Resource\\1.Depth_testing.vs","..\\Resource\\1.Depth_testing.fs");
+    Shader ourShader("..\\Resources\\DepthTest.vs","..\\Resources\\DepthTest.fs");
+    
+    // Init VAO, VBO for Cube
+    unsigned int cubeVAO, cubeVBO;
+	glGenBuffers(1, &cubeVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &cubeVAO);
+	glBindVertexArray(cubeVAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void*)0);
+    glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void*)(3 * sizeof(GL_FLOAT)));
+    glBindVertexArray(0);
+    
+    // load textures
+    // -------------
+    unsigned int cubeTexture  = loadTexture("..\\Resources\\marble.jpg");
 
+    // shader configuration
+	ourShader.Use();
+	ourShader.setInt("ourTexture", 0);
     while(!window.shouldClose())
     {
         keys = window.getKeyPress();
         do_movement();
         // render
         // -----
-        glClearColor(0.2f, 0.3f, 0.4f, 0.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+		ourShader.Use();
+        glm::mat4 model;
+		glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(45.0f, (float)(SCR_W/SCR_H), 0.1f, 100.0f);;
+
+		ourShader.setMat4("view", view);
+		ourShader.setMat4("projection", projection);
+
+        // cubes
+        glBindVertexArray(cubeVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, cubeTexture);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		ourShader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        window.swapBuffers();
+        window.pollEvents();
     }
+}
+
+void do_movement()
+{
+    if (keys[GLFW_KEY_W])
+    {
+        camera.ProcessKeyboard(Camera_Movement::FORWARD);
+    }
+    if (keys[GLFW_KEY_S])
+    {
+        camera.ProcessKeyboard(Camera_Movement::BACKWARD);
+    }
+    if (keys[GLFW_KEY_A])
+    {
+        camera.ProcessKeyboard(Camera_Movement::LEFT);
+    }
+    if (keys[GLFW_KEY_D])
+    {
+        camera.ProcessKeyboard(Camera_Movement::RIGHT);
+    }
+    if (keys[GLFW_KEY_LEFT])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_LEFT);
+    }
+    if (keys[GLFW_KEY_RIGHT])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_RIGHT);
+    }
+    if (keys[GLFW_KEY_UP])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_UP);
+    }
+    if (keys[GLFW_KEY_DOWN])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_DOWN);
+    }
+}
+
+// utility function for loading a 2D texture from file
+// ---------------------------------------------------
+unsigned int loadTexture(char const *path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
 }
