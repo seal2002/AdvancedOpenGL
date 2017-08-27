923777f9ed1ab1b242edeba2e52171853bbc966f
seal2002 <hai.phannhut@gameloft.com>
2017-08-27 22:08:15 +0700

    Update Load texture with Transparent texture

--- Libraries/common/LoadTexture.h
+++ Libraries/common/LoadTexture.h
@@ -9,7 +9,7 @@ unsigned int loadTexture(char const *path)
 {
     unsigned int textureID;
     glGenTextures(1, &textureID);
-
+    bool isAlpha = false;
     int width, height, nrComponents;
     stbi_set_flip_vertically_on_load(1);
     unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
@@ -21,19 +21,27 @@ unsigned int loadTexture(char const *path)
         else if (nrComponents == 3)
             format = GL_RGB;
         else if (nrComponents == 4)
+        {
             format = GL_RGBA;
+            isAlpha = true;
+        }
 
         glBindTexture(GL_TEXTURE_2D, textureID);
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-        /*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        This is correct code for Alpha Texture, we need to clamp to ede 
-        to avoid the semi-transperent colored border*/
+        if (isAlpha)
+        {
+            /*This is correct code for Alpha Texture, we need to clamp to ede
+            to avoid the semi-transperent colored border*/
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        }
+        else
+        {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        }
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
