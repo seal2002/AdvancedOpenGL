f626c9fec81b712d896a1efd7e2103ffabf1a466
seal2002 <phannhuthai@gmail.com>
2017-09-16 20:36:00 +0700

    - Remove unused file * Finish Demo UniformBuffer

--- Projects/8.2.UniformBufferExample/UniformBufferDemo.cpp
+++ Projects/8.2.UniformBufferExample/UniformBufferDemo.cpp
@@ -21,73 +21,73 @@ Camera camera(glm::vec3(0.0f, 0.5f, 3.0f));
 bool* keys;
 
 float cubeVertices[] = {
-	// positions         
-	-0.5f, -0.5f, -0.5f,
-	0.5f, -0.5f, -0.5f,
-	0.5f,  0.5f, -0.5f,
-	0.5f,  0.5f, -0.5f,
-	-0.5f,  0.5f, -0.5f,
-	-0.5f, -0.5f, -0.5f,
+    // positions
+    -0.5f, -0.5f, -0.5f,
+    0.5f, -0.5f, -0.5f,
+    0.5f,  0.5f, -0.5f,
+    0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
 
-	-0.5f, -0.5f,  0.5f,
-	0.5f, -0.5f,  0.5f,
-	0.5f,  0.5f,  0.5f,
-	0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f,  0.5f,
-	-0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+    0.5f, -0.5f,  0.5f,
+    0.5f,  0.5f,  0.5f,
+    0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
 
-	-0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f, -0.5f,
-	-0.5f, -0.5f, -0.5f,
-	-0.5f, -0.5f, -0.5f,
-	-0.5f, -0.5f,  0.5f,
-	-0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
 
-	0.5f,  0.5f,  0.5f,
-	0.5f,  0.5f, -0.5f,
-	0.5f, -0.5f, -0.5f,
-	0.5f, -0.5f, -0.5f,
-	0.5f, -0.5f,  0.5f,
-	0.5f,  0.5f,  0.5f,
+    0.5f,  0.5f,  0.5f,
+    0.5f,  0.5f, -0.5f,
+    0.5f, -0.5f, -0.5f,
+    0.5f, -0.5f, -0.5f,
+    0.5f, -0.5f,  0.5f,
+    0.5f,  0.5f,  0.5f,
 
-	-0.5f, -0.5f, -0.5f,
-	0.5f, -0.5f, -0.5f,
-	0.5f, -0.5f,  0.5f,
-	0.5f, -0.5f,  0.5f,
-	-0.5f, -0.5f,  0.5f,
-	-0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    0.5f, -0.5f, -0.5f,
+    0.5f, -0.5f,  0.5f,
+    0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f, -0.5f,
 
-	-0.5f,  0.5f, -0.5f,
-	0.5f,  0.5f, -0.5f,
-	0.5f,  0.5f,  0.5f,
-	0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f,  0.5f,
-	-0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    0.5f,  0.5f, -0.5f,
+    0.5f,  0.5f,  0.5f,
+    0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
 };
 
 int main()
 {
     Window window(SCR_W, SCR_H, "Advance GLSL - Uniform Buffer Demo");
 
-	glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);
      // Load Shader
-	string pathVS(PATH);
-	pathVS += "\\cube.vs";
+    string pathVS(PATH);
+    pathVS += "\\cube.vs";
 
-	string pathFSRed(PATH);
-	pathFSRed += "\\cubeRed.fs";
+    string pathFSRed(PATH);
+    pathFSRed += "\\cubeRed.fs";
     Shader shaderRed(pathVS.c_str(), pathFSRed.c_str());
 
-	string pathFSGreen(PATH);
-	pathFSGreen += "\\cubeGreen.fs";
+    string pathFSGreen(PATH);
+    pathFSGreen += "\\cubeGreen.fs";
     Shader shaderGreen(pathVS.c_str(), pathFSGreen.c_str());
 
-	string pathFSBlue(PATH);
-	pathFSBlue += "\\cubeBlue.fs";
+    string pathFSBlue(PATH);
+    pathFSBlue += "\\cubeBlue.fs";
     Shader shaderBlue(pathVS.c_str(), pathFSBlue.c_str());
 
-	string pathFSYellow(PATH);
-	pathFSYellow += "\\cubeYellow.fs";
+    string pathFSYellow(PATH);
+    pathFSYellow += "\\cubeYellow.fs";
     Shader shaderYellow(pathVS.c_str(), pathFSYellow.c_str());
 
     // Init VAO, VBO for Cube
@@ -102,36 +102,36 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
 
     glBindVertexArray(0);
-    
+
     // Set uniform block of vertex shader
     unsigned int uniformBlockIndexRed = glGetUniformBlockIndex(shaderRed.Program, "Matrices");
     unsigned int uniformBlockIndexGreen = glGetUniformBlockIndex(shaderGreen.Program, "Matrices");
     unsigned int uniformBlockIndexBlue = glGetUniformBlockIndex(shaderBlue.Program, "Matrices");
     unsigned int uniformBlockIndexYellow = glGetUniformBlockIndex(shaderYellow.Program, "Matrices");
-    
+
     // Set uniform block binding
-	glUniformBlockBinding(shaderRed.Program, uniformBlockIndexRed, 0);
+    glUniformBlockBinding(shaderRed.Program, uniformBlockIndexRed, 0);
     glUniformBlockBinding(shaderGreen.Program, uniformBlockIndexGreen, 0);
     glUniformBlockBinding(shaderBlue.Program, uniformBlockIndexBlue, 0);
     glUniformBlockBinding(shaderYellow.Program, uniformBlockIndexYellow, 0);
-    
+
     unsigned int uboMatrices;
     glGenBuffers(1, &uboMatrices);
-    
+
     glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
     glBufferData(GL_UNIFORM_BUFFER, 2 * sizeof(glm::mat4), NULL, GL_STATIC_DRAW);
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
-    
-	glBindBufferRange(GL_UNIFORM_BUFFER, 0, uboMatrices, 0, 2 * sizeof(glm::mat4));
-    
+
+    glBindBufferRange(GL_UNIFORM_BUFFER, 0, uboMatrices, 0, 2 * sizeof(glm::mat4));
+
     // End to construct for Uniform Buffer
 
-	// set projection to uniform buffer
-	glm::mat4 projection = glm::perspective(45.0f, (float)SCR_W / (float)SCR_H, 0.1f, 100.0f);
-	glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
-	glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &projection[0][0]);
-	glBindBuffer(GL_UNIFORM_BUFFER, 0);
-	glCheckError();
+    // set projection to uniform buffer
+    glm::mat4 projection = glm::perspective(45.0f, (float)SCR_W / (float)SCR_H, 0.1f, 100.0f);
+    glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &projection[0][0]);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    glCheckError();
 
     while(!window.shouldClose())
     {
@@ -144,21 +144,46 @@ int main()
         // set view to uniform buffer
         glm::mat4 view = camera.GetViewMatrix();
         glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
-        glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &view[0][0]);
+        glBufferSubData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), sizeof(glm::mat4), &view[0][0]);
         glBindBuffer(GL_UNIFORM_BUFFER, 0);
-        
 
-		glBindVertexArray(cubeVAO);
+
+        glBindVertexArray(cubeVAO);
+        // RED
         shaderRed.Use();
         glm::mat4 model;
-        //model = glm::translate(model, glm::vec3(0.75f, 0.75f, -1.0f));
-        //shaderRed.setMat4("model", model);
+        model = glm::translate(model, glm::vec3(-0.75f, 0.75f, -1.0f));
+        shaderRed.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
-		glCheckError();
+
+        // GREEN
+        shaderGreen.Use();
+        model = glm::mat4();
+        model = glm::translate(model, glm::vec3(0.75f, 0.75f, -1.0f)); // move top-right
+        shaderGreen.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // BLUE
+        shaderBlue.Use();
+        model = glm::mat4();
+        model = glm::translate(model, glm::vec3(0.75f, -0.75f, -1.0f)); // move top-right
+        shaderBlue.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // YELLOW
+        shaderYellow.Use();
+        model = glm::mat4();
+        model = glm::translate(model, glm::vec3(-0.75f, -0.75f, -1.0f)); // move top-right
+        shaderYellow.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
         glBindVertexArray(0);
         window.pollEvents();
         window.swapBuffers();
     }
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteBuffers(1, &cubeVBO);
+    glDeleteBuffers(1, &uboMatrices);
 }
 
 void do_movement()
