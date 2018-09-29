953db514e7a6bb759108cb232ba269e2deda023e
Seal2002 <phannhuthai@gmail.com>
2018-09-29 17:05:34 +0700

    Guard check for key input

--- Projects/MainWindow/MainWindow.cpp
+++ Projects/MainWindow/MainWindow.cpp
@@ -133,6 +133,8 @@ namespace OpenGLWindow
 
     void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
     {
+		if (key < 0 || key > GLFW_KEY_LAST)
+			return;
         if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
             glfwSetWindowShouldClose(window, GL_TRUE);
         if (action == GLFW_PRESS)
