59b2381d2afe6319424bb80b6c2d91e2999dd347
Phan Nhut Hai <hai.phannhut@gameloft.com>
2018-10-08 10:07:33 +0700

    Correct Binding to Framebuffer

--- Projects/5.1.FrameBuffers/FrameBuffers.cpp
+++ Projects/5.1.FrameBuffers/FrameBuffers.cpp
@@ -205,7 +205,7 @@ int main()
         // -----
 
         // first pass
-        glBindFramebuffer(GL_FRAMEBUFFER, rbo);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
